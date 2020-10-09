262cc2b53aa7b43ebd51add14673a50573f2d5e0
JasemDuncan <jasemvalencia@gmail.com>
2020-10-08 23:44:47 -0500

    Min Max

---
+++ MinMax.rb
@@ -0,0 +1,14 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr)
+
+
+end
+
+arr = gets.rstrip.split(' ').map(&:to_i)
+
+miniMaxSum arr
