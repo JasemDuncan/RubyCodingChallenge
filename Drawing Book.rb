7e304ac9f73839c68c8966c4f5c1089e99610a95
JasemDuncan <jasemvalencia@gmail.com>
2020-10-11 23:25:17 -0500

    Drawing Book incomplete

---
+++ Drawing Book.rb
@@ -0,0 +1,24 @@
+#!/bin/ruby
+
+#
+# Complete the pageCount function below.
+#
+def pageCount(n, p)
+    #
+    # Write your code here.
+    #
+
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+n = gets.to_i
+
+p = gets.to_i
+
+result = pageCount n, p
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
