79f1187f7f3fd72e722ae95aeb95dbaab9e68cb2
Jasem Duncan <jasemvalencia@gmail.com>
2022-11-23 08:34:14 -0500

    finish coding challenge

--- SubString.rb
+++ SubString.rb
@@ -1,16 +1,23 @@
+# https://www.theodinproject.com/lessons/ruby-sub-strings
+# Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
+
 require 'pry-byebug'
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(word,dictionary)
   result = Hash.new(0)
-  lowered_text = word.downcase
+  # puts result
+  # lowered_text = word.downcase
 
   dictionary.each do |palabra|
-    matches=lowered_text.scan(palabra)
-    print matches
+    matches=word.downcase.scan(palabra).length
+    result[palabra]=matches if matches!=0
+    # puts matches
+
   end
   
-
+return  result
 end
 
-substrings("below",dictionary)
+puts substrings("below",dictionary)
+# puts "Jasema".scan("a").length
